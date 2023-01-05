f5709efd306436a59cf3c30ba05530267f790f30
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-05 17:53:06 -0500

    Fix bug with null image

--- src/Components/ArticlesContainer/ArticlesContainer.js
+++ src/Components/ArticlesContainer/ArticlesContainer.js
@@ -1,20 +1,24 @@
 import './ArticlesContainer.css';
 import ArticleCard from '../ArticleCard/ArticleCard';
+import nytSymbol from '../../assets/Symbol-New-York-Times.webp';
 
 const ArticlesContainer = ({ articles }) => {
-  console.log(articles);
+  if (!articles) {
+    return <h1>Loading...</h1>;
+  }
 
   const cards = articles.map(article => {
     return (
       <ArticleCard
         key={ article.published_date }
-        image={ article.multimedia[1].url }
+        image={ !article.multimedia ? nytSymbol : article.multimedia[1].url }
         title={ article.title }
         summary={ article.abstract }
         author={ article.byline }
       />
     );
   });
+
   return (
     <section className='articles-container'>
       { cards }
