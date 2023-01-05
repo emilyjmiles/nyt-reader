a43bdc38447ac74344c12a085293cd7eee50fe68
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-05 17:52:19 -0500

    Remove second fetch call that is no longer being used in detalied view

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchAllData = (section) => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=cy0m0wurwwUOUU5jjyglUFQDQasolmu5
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=w5xv1NouT0t0kG7pUWSLa5Il5c0E9p1H
   `)
     .then((response) => {
       if (!response.ok) {
@@ -14,20 +14,4 @@ const fetchAllData = (section) => {
     });
 };
 
-const fetchSingleArticle = (title) => {
-  return fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${title}&api-key=cy0m0wurwwUOUU5jjyglUFQDQasolmu5
-  `)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `This is an HTTP error: The status is ${response.status} `
-        );
-      }
-      return response.json();
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
-
-export { fetchAllData, fetchSingleArticle };;;
+export { fetchAllData };;;
