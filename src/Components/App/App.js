b3d117ba3f3b81dc6fa54e9274989729c7d36d30
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-06 16:17:56 -0500

    Add search methods for on change

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,23 +6,61 @@ import './App.css';
 import Header from '../Header/Header';
 import ArticlesContainer from '../ArticlesContainer/ArticlesContainer';
 import ArticleDetails from '../ArticleDetails/ArticleDetails';
+import SearchForm from '../SearchForm/SearchForm';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [sortValue, setSortValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [articlesBySection, setArticlesBySection] = useState([]);
+  const [articlesSearch, setArticlesSearch] = useState([]);
 
   useEffect(() => {
     fetchAllData('home')
       .then(data => setArticles(data.results));
   }, []);
 
+  const handleSearch = (event) => {
+    const search = event.target.value.toLowerCase();
+    setSearchValue(search);
+  };
+
+  const handleSort = (event) => {
+    const section = event.target.value.toLowerCase();
+    setSortValue(section);
+  };
+
+  // articles.filter(article => {
+  //   if (article.section === sortValue || article.subsection === sortValue) {
+  //     setArticlesBySection([...articlesBySection, article]);
+  //   }
+  //   return articlesBySection;
+  // });
+
+  const clearInputs = (event) => {
+    event.preventDefault();
+    setSortValue('');
+    setSearchValue('');
+  };
+
   return (
     <main>
       <Header />
       <Switch>
-        <Route
-          exact path='/'
-          render={ () => <ArticlesContainer articles={ articles } /> }
-        />
+        <Route exact path='/'>
+          <section className='main-page'>
+            <h1 className='top-stories'>Today's Top Stories</h1>
+            <SearchForm
+              articles={ articles }
+              searchValue={ searchValue }
+              sortValue={ sortValue }
+              handleSearch={ handleSearch }
+              handleSort={ handleSort }
+              clearInputs={ clearInputs }
+            />
+            <ArticlesContainer articles={ articles } />
+          </section>
+        </Route>
         <Route
           path='/:title'
           render={ ({ match }) => {
