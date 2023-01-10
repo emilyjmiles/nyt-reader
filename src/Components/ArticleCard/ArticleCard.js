063f77d3855ca1762aa763c1e4883e198cd54b78
Emily Miles <ms.emily.j.miles@gmail.com>
2023-01-10 15:15:00 -0500

    Add explicit dimensions to images

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './ArticleCard.css';
 
-const ArticleCard = ({ image, title, date }) => {
+const ArticleCard = ({ id, image, title, date }) => {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const getDate = date.split('T')[0];
   const reverseDate = getDate.split('-').reverse();
@@ -14,8 +14,14 @@ const ArticleCard = ({ image, title, date }) => {
   const formatDate = `${removeZeros[0]} ${months[parseInt(removeZeros[1] - 1)]} ${removeZeros[2]}`;
 
   return (
-    <Link to={ date } className='article-card'>
-      <img src={ image } alt={ title } className='article-image' />
+    <Link to={ id } className='article-card'>
+      <img
+        src={ image.url }
+        alt={ title }
+        className='article-image'
+        width={ image.width }
+        height={ image.height }
+      />
       <div className='article-snippet'>
         <h2 className='title'>{ title }</h2>
         <p>{ formatDate }</p>
